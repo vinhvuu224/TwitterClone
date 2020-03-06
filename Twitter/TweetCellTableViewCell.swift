7bf6a149000eb833221505dc85e82e051f971c82
vinhvu <vinhvu224@gmail.com>
2020-03-05 21:28:53 -0800

    first commit

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Vinh Vu on 3/5/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
